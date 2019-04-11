8d27e908c75a965045443a6e081447e874da751c
Thiagtao da Silva Adriano <tadriano@band.com.br>
2019-04-11 20:00:15 -0300

    add scene

--- Assets/Scripts/bigvegas.cs
+++ Assets/Scripts/bigvegas.cs
@@ -7,14 +7,18 @@ public class bigvegas : MonoBehaviour
     // Start is called before the first frame update
 
     Animator anim;
+    AudioSource audioData;
+
     void Start()
     {
+        audioData = GetComponent<AudioSource>();
         anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     public void Dance()
     {
+        audioData.Play(0);
         anim.SetBool("dance", true);
     }
 }
